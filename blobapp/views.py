30b78512d335761613b80fcd26c41cb0516c7b74
Chirasree Mandal <chirasree.mandal@gmail.com>
2019-10-26 15:19:24 -0700

    add basic rendering behavior for get requests to main page, start writing static resources for main page

--- blobapp/views.py
+++ blobapp/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from django.http import HttpResponse, HttpResponseNotFound
+#from utils.py import fileSaver
+
+
+
+@require_http_methods(["GET", "POST"])
+def handle_req(request):
+    """Main handler function that accepts POST and GET requests only"""
+
+    if request.method == "GET":
+        #proceed with get request rendering
+        return render(request, 'templates/index.html')
+    if request.method == "POST":
+        print("hi")
+        #read formdata and make fileUpload object
+        #send to fileSaver function
+        return render(request, "templates/index.html", {"jobstatus": "Success"})
+    else:
+        return HttpResponseNotFound('<h1>The resource you requested was not available at this time.</h1>')
 
 
 def submit():
