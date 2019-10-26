e5bae3fa9d281677fd63e8308d53ac649d2a038d
Chirasree Mandal <chirasree.mandal@gmail.com>
2019-10-26 15:42:38 -0700

    add url configurations in blobapp and include them in top-level whatsmyblob url routes

--- whatsmyblob/urls.py
+++ whatsmyblob/urls.py
@@ -14,13 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from blobapp import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', views.submit),
-    path('results/', views.result),
-    path('status', views.status)
+    path('', include('blobapp.urls')),
 ]
