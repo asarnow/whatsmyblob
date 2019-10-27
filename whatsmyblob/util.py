4f21d54d2f884f2f30ae1453d2be85f8d605697f
Daniel Asarnow <dasarnow@gmail.com>
2019-10-26 20:35:44 -0700

    Basics to try search

--- whatsmyblob/util.py
+++ whatsmyblob/util.py
@@ -1,12 +1,16 @@
 # Copyright (C) 2018 Daniel Asarnow
 # University of California, San Francisco
 import mrcfile
-import os.path
+import os
 from whatsmyblob import constants
+from whatsmyblob import job_control
+from whatsmyblob import sus
+from whatsmyblob import neighbor_tree
+from whatsmyblob import run_colores
 
 
-def handle_upload_mrc(jobid, upload):
-    dest = os.path.join(constants.TEMP_ROOT, jobid, "query.mrc")
+def handle_upload_mrc(job, upload):
+    dest = os.path.join(constants.TEMP_ROOT, str(job.id), "query.mrc")
     with open(dest, 'wb+') as f:
         for c in upload.chunks():
             f.write(c)
@@ -17,3 +21,15 @@ def handle_upload_mrc(jobid, upload):
         return False
     return True
 
+
+def run_search(job):
+    query = job.query_map_file.map_file.url
+    query_pc = sus.sus(query, samples=256)
+    query_distmat = neighbor_tree.get_distmat(query_pc)
+    query_inv_cdf = neighbor_tree.get_inv_cdf(query_distmat)
+    tree = neighbor_tree.load_balltree("nn_tree_1dgw.pkl")
+    ids = neighbor_tree.np.load("nn_tree_1dgw_ids.npy")
+    hits = neighbor_tree.query_bt(query_inv_cdf, tree, ids, k=10)
+    jobdir = job_control.create_tmp_dir(constants.TEMP_ROOT, job.id)
+    run_colores.run_colores(query, hits, constants.CATHDB, output="result.json")
+    return hits
