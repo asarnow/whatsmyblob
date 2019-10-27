2286a51ad3c5281fe2d8a2ccb31a91e7bb645d3a
Daniel Asarnow <dasarnow@gmail.com>
2019-10-27 13:53:49 -0700

    Code to generate structure db.

---
+++ whatsmyblob/generate_training_data.py
@@ -0,0 +1,111 @@
+#! /usr/bin/env python3
+# Copyright (C) 2018 Eugene Palovcak
+# University of California, San Francisco
+#
+# Function to sample distance matrices from protein PDBs 
+# given a large database of such proteins. 
+#
+# To train NN, we need anchor, positive, and negative examples
+#
+# Uses nearest-neighbor search and 1D wasserstein to find 
+# harder negative examples
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import numpy as np
+import h5py as h5
+import os
+import pickle
+from glob import glob
+from scipy.spatial.distance import squareform
+from scipy.spatial.distance import pdist
+from numpy.random import normal
+from tqdm import tqdm
+from prody import parsePDB
+from prody import confProDy
+from . import neighbor_tree
+
+
+def make_distmats(protein_structures="/storage/datasets/cath/dompdb/*", 
+                  output_file='training_data.hdf'):
+    """ Creates and HDF5 file with samples distance matrices and 1D CDFs 
+        protein_structures: path to all domains to parse
+    """
+
+
+    dbank = h5.File(output_file, 'a')
+    dist_mats = dbank.create_group('dist_mats')
+    confProDy(verbosity='none')
+
+    pdb_list = glob(protein_structures)
+
+    for pdb_file in tqdm(pdb_list, desc="Dist-mats", total=len(pdb_list)):
+        try:
+            pdb = parsePDB(pdb_file).select('name CA CB')
+            pdb_id = pdb_file.split("/")[-1]
+
+            coords = pdb.getCoords()
+            coords -= coords.mean(axis=0)
+
+            dists = pdist(coords)
+            dm = squareform(dists)
+            dist_mats[pdb_id] = dm
+
+        except:
+            print(f"File {pdb} was not properly parsed...")
+            pass
+
+
+def get_triplets(triplet_file="triplets_noise3.csv",
+                 id_file="nn_tree_1dgw_ids.npy",
+                 protein_structures="./dompdb/*",
+                 balltree_file="nn_tree_1dgw.pkl", noise=3):
+    """ Generate triplets for training """
+
+    # Load id file and ball tree
+    ids = np.load(id_file)
+    with open(balltree_file, "rb") as f:
+        tree = pickle.loads(f.read())
+
+    confProDy(verbosity='none')
+    pdb_list = glob(protein_structures)
+
+    with open(triplet_file, "w") as handle:
+        for pdb_file in tqdm(pdb_list, desc="Triplets", total=len(pdb_list)):
+            try:
+                pdb = parsePDB(pdb_file).select('name CA CB')
+                pdb_id = pdb_file.split("/")[-1]
+
+                coords = pdb.getCoords()
+                coords -= coords.mean(axis=0)
+
+                coords += normal(scale=noise, size=coords.shape)
+
+                dists = pdist(coords)
+                dm = squareform(dists)
+
+                shape_dist = neighbor_tree.get_inv_cdf(dm)
+                nns = neighbor_tree.query_bt(shape_dist, tree, ids)
+
+                for nn in nns[0]:
+                    if nn != pdb_id:
+                        triplet = f"{pdb_id},{pdb_id},{nn}\n"
+                        handle.write(triplet)
+
+            except:
+                print(f"File {pdb} was not properly parsed...")
+                pass
+
+if __name__=="__main__":
+    make_distmats()
+
--- whatsmyblob/neighbor_tree.py
+++ whatsmyblob/neighbor_tree.py
@@ -1,8 +1,58 @@
 import numpy as np
 import pickle
+from h5py import File
 from scipy.spatial.distance import pdist
 from scipy.spatial.distance import squareform
 from sklearn.neighbors import BallTree
+from tqdm import tqdm
+
+
+def make_1GW(input_file="training_data.hdf", samples=64):
+    """ Reads an HDF file with distance matrices and computes 1D GWs """
+
+    # Load distance matrices
+    dbank = File(input_file, "a")
+    dist_mats = dbank['dist_mats']
+    pdb_list = [key for key in dist_mats.keys()]
+
+    # Create 1D GW distances
+    output_file = input_file.split(".")[0] + "_GW.pkl"
+    shape_dists = {}
+
+    for pdb in tqdm(pdb_list, desc="Shape-dists", total=len(pdb_list)):
+        try:
+            dist_mat = dist_mats[pdb]
+            shape_dist = get_inv_cdf(dist_mat, samples)
+            shape_dists[pdb] = shape_dist
+
+        except:
+            print(f'{pdb} is corrupt or already has a shape distribution...')
+            pass
+
+    return shape_dists
+
+
+def make_GW_tree(shape_dists, treename="nn_tree_1dgw.pkl"):
+    """ Make tree for nearest neighbors """
+
+    # Get PDB ids and save the array
+    ids = np.array([pdb for pdb in shape_dists.keys()])
+    id_file = treename.split(".")[0] + "_ids.npy"
+    np.save(id_file, ids)
+
+    # Construct and save the tree
+    shape_dists_array = np.array([shape_dists[pdb] for pdb in ids])
+    tree = BallTree(shape_dists_array)
+
+    # Test
+    n = 20
+    query = shape_dists[ids[n]]
+    print(ids[n], query_bt(query, tree, ids))
+
+    # Dump the pickle
+    tree_pickle = pickle.dumps(tree)
+    with open(treename, "wb") as handle:
+         handle.write(tree_pickle)
 
 
 def get_distmat(coords):
@@ -37,3 +87,7 @@ def query_bt(query, bt, ind2id, k=10):
     # return [(ids[ind],dis) for ind,dis in zip(inds[0],dists[0])]
     return ids, dists[0]
 
+
+if __name__ == "__main__":
+    shape_dists = make_1GW(samples=64)
+    GW_tree = make_GW_tree(shape_dists)
