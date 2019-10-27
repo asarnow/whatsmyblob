fc2cad03653f4e8b8375a792d552e172ac60a733
Daniel Asarnow <dasarnow@gmail.com>
2019-10-26 17:12:31 -0700

    Only job model is needed

--- blobapp/models.py
+++ blobapp/models.py
@@ -5,10 +5,9 @@ class Job(models.Model):
     submit_time = models.DateTimeField()
     start_time = models.DateTimeField()
     completion_time = models.DateTimeField()
-    query_map_file = models.CharField(max_length=100)
+    query_map_file = models.CharField(max_length=300)
     query_points_file = models.CharField(max_length=100)
+    stage = models.IntegerField()
+    result_json_file = models.CharField(max_length=100)
 
 
-class Result(models.Model):
-    job = models.ForeignKey(Job, on_delete=models.CASCADE)
-    json_file = models.CharField(max_length=100)
